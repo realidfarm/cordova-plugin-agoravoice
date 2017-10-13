33a9c5f3398fe81e7f7c07fce5e6faec3d1481b6
taofengno1 <taofeng_ls@sina.com>
2017-10-13 14:15:27 +0800

    修改js变量名

--- www/agoravoice.js
+++ www/agoravoice.js
@@ -1,8 +1,8 @@
 var exec = require('cordova/exec');
 
 exports.connect = function(token, success, error) {
-    exec(success, error, "RCcall", "connect", [token]);
+    exec(success, error, "Agoravoice", "connect", [token]);
 };
 exports.voiceCall = function(success, error) {
-    exec(success, error, "RCcall", "voiceCall", []);
+    exec(success, error, "Agoravoice", "voiceCall", []);
 };
